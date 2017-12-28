f33c58829521ee5c7c25dd0be95410b35e1d7436
Michele Giugliano <mgiugliano@users.noreply.github.com>
2017-12-28 17:57:16 +0100

    Add files via upload
    Bug fix

--- CWpairs.c
+++ CWpairs.c
@@ -1,9 +1,9 @@
 //
 // CWpairs - My own Interactive and Adaptive
-//            Morse trainer for Single Characters
+//            Morse trainer for Pairs of Characters
 //
-// 22/1/2017 - (c) Michele Giugliano (mgiugliano@gmail.com)
-// Compile with: gcc -o CWsingle CWsingle.c -O
+// 23/12/2017 - (c) Michele Giugliano (mgiugliano@gmail.com)
+// Compile with: gcc -o CWpairs CWpairs.c -O
 //
 
 #include "CWpairs.h"
@@ -15,93 +15,107 @@ int main(int argc, char *argv[]) { // main()
  char err[40];                  // Will contain the command to play the error mp3 sound
  int n, n1, n2;                 // Index of the current symbol to play and to be copied
  char current1, current2;       // Current symbol to play and to be copied
+ int nerr;						                // Number of subsequent errors - for voice cue
+ char current;                  // Current symbol to play and to be copied
+ char temp;						               // Temporary variable to allow for repetition of a character
  int made_an_error;             // Boolean variable to distinguish wrong from right.
+ int tmp;                       // Temporary variable to intercept the repetition request.
  struct sigaction sigIntHandler;// To catch the CTRL-C event.
 
   srand((unsigned)time(NULL)); // Initialise seed of random n generator, with curren time
   rand();            // Discarded: the first rand() returns always the same value
 
-  Nsyms = argc-1;   // Global var, containing the number of pairs entered by the user
+  Nsyms = argc-1;   // Global var, containing the number of symbols entered by the user
   syms  = argv;     // Global var, containing the symbols entered by the user
   Ntot  = 0;        // Global var, containing the total number of "trials"
   Nerr  = 0;        // Global var, containing the total number of "errors"
   p_baseline = 1. / ((double) Nsyms*Nsyms); // Equal probabilities for each symbol (initially at least)
 
 
- if (argc<2) {                // If called without argument, print the correct usage and
-  printf(USAGE, argv[0]);     // exit silently. Otherwise the number of symbols
-    return 0;                 // entered is (argc - 1) and the symbols are argv[1], argv[2], ...
+ if (argc<2) {            // If called without argument, print the correct usage and
+	printf(USAGE, argv[0]);	 // exit silently. Otherwise the number of symbols
+    return 0;					        // entered is (argc - 1) and the symbols are argv[1], argv[2], ...
   }
   system("clear");
   printf("Welcome to CWpairs! (c) 2017 Michele Giugliano\n");
-  printf("Press any key to start and CTRL-C to terminate at any moment.\nSymbols for this session: ");
+  printf("Symbols for this session (%d): ",Nsyms);
   for (n=0; n<Nsyms; n++)
    printf("%c ", syms[n+1][0]);
-  printf("\n\n");
+  printf("\n\nPress any key to start and CTRL-C to terminate at any moment.\n\n");
 
-  P    = (double **) calloc(Nsyms, sizeof(double *));  // Memory allocation for P
+  P    = (double **) calloc(Nsyms, sizeof(double *));	// Memory allocation for P
   for (n1=0; n1<Nsyms; n1++) {
-   P[n1] = (double *) calloc(Nsyms, sizeof(double));  // Memory allocation for P
-   for (n2=0; n2<Nsyms; n2++)
-    P[n1][n2] = p_baseline;       // All probabilities are initialised and set equal.
-  }
+     P[n1] = (double *) calloc(Nsyms, sizeof(double));  // Memory allocation for P
+     for (n2=0; n2<Nsyms; n2++)
+      P[n1][n2] = p_baseline;       // All probabilities are initialised and set equal.
+    }
 
   // The system command to play an error sound, in case of a mistake, is prepared here:
-  sprintf(err, "afplay ./wrong.mp3"); // do NOT use & (so that the proper pause is granted)
+  sprintf(err, "afplay ./wrong.mp3"); // do NOT use & (so that a proper pause is granted)
 
   sigIntHandler.sa_handler = my_handler;  // Catching the interrupts and later free the memory
   sigemptyset(&sigIntHandler.sa_mask);    // Catching the interrupts and later free the memory
   sigIntHandler.sa_flags = 0;             // Catching the interrupts and later free the memory
 
-  getch();
+  getch();    // The first character is wasted: the user presses a key to start.
   while (1) { //-=-=-= INFINITE LOOP -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
-    sigaction(SIGINT, &sigIntHandler, NULL);    // Let's prepare to catch CTRL-C (to free memory)
-    made_an_error = 0;                          // Boolean var initialised, to catch user's errors
+    sigaction(SIGINT, &sigIntHandler, NULL); // Let's prepare to catch CTRL-C (to free memory)
+    made_an_error = 0;					                  // Boolean var initialised, to catch user's errors
 
     Ntot++;                       // The counter of the total number of trials is updated here
 
-    normaliseP2(P, Nsyms);        // Normalise the symbols occurrence probabilities to sum 1
-    n = rand_event2(P, Nsyms);    // One pair of letters is generated "randomly" with probabilities P[]
+	  normaliseP2(P, Nsyms);				    	// Normalise the symbols occurrence probabilities to sum 1
+ 	  n = rand_event2(P, Nsyms); 		  // One letter is generated "randomly" with probabilities P[]
     n1 = n/Nsyms;
     n2 = n%Nsyms;
     current1 = argv[n1+1][0];     // First symbol taken from the list of those provided by the user
     current2 = argv[n2+1][0];     // Second symbol taken from the list of those provided by the user
 
-    // Prepare now for the command to play audio files (with some exceptions in the file names)
-       switch (current1) {
-      case '?':
-        if (current2=='?')      sprintf(cmd, "afplay ./pairs/__.mp3 &");
-        else if (current2=='/') sprintf(cmd, "afplay ./pairs/_+.mp3 &");
-        else                    sprintf(cmd, "afplay ./pairs/_%c.mp3 &", current2);
-        break;
-      case '/':
-        if (current2=='?')      sprintf(cmd, "afplay ./pairs/+_.mp3 &");
-        else if (current2=='/') sprintf(cmd, "afplay ./pairs/++.mp3 &");
-        else                    sprintf(cmd, "afplay ./pairs/+%c.mp3 &", current2);
-        break;
-      default:
-        if (current2=='?')      sprintf(cmd, "afplay ./pairs/%c_.mp3 &", current1);
-        else if (current2=='/') sprintf(cmd, "afplay ./pairs/%c+.mp3 &", current1);
-        else                    sprintf(cmd, "afplay ./pairs/%c%c.mp3 &",current1, current2);
-    }
+   //sprintf(cmd, "afplay ./pairs/%c%c.mp3 &",current1, current2);
 
-    //printf("%s\n", cmd);
-    system(cmd);                     // Launch the command by a system shell call
-    printf("?>    \r");  fflush(stdout);// Prints on screen a prompt
-    while (!compare(current1, current2)) {  // Blocks execution until user press the right button on keyboard
-        printf("X>    \r");  fflush(stdout); // Feedback is given to the user
-        made_an_error = 1;      // If we are here, at least one error has been made.
-        Nerr++;                 // The counter of the total number of mistakes is updated here
-        system(err);            // The error sound is played to the user
-        system(cmd);            // The symbol is played again to the user
-        P[n1][n2] *= 2.;       // The corresponding occurrence probability is doubled (i.e. repetita iuvant)
+   // Prepare now for the command to play audio files (with some exceptions in the file names)
+        switch (current1) {
+       case '?':
+         if (current2=='?')      sprintf(cmd, "afplay ./pairs/__.mp3 &");
+         else if (current2=='/') sprintf(cmd, "afplay ./pairs/_+.mp3 &");
+         else                    sprintf(cmd, "afplay ./pairs/_%c.mp3 &", current2);
+         break;
+       case '/':
+         if (current2=='?')      sprintf(cmd, "afplay ./pairs/+_.mp3 &");
+         else if (current2=='/') sprintf(cmd, "afplay ./pairs/++.mp3 &");
+         else                    sprintf(cmd, "afplay ./pairs/+%c.mp3 &", current2);
+         break;
+       default:
+         if (current2=='?')      sprintf(cmd, "afplay ./pairs/%c_.mp3 &", current1);
+         else if (current2=='/') sprintf(cmd, "afplay ./pairs/%c+.mp3 &", current1);
+         else                    sprintf(cmd, "afplay ./pairs/%c%c.mp3 &",current1, current2);
+     }
 
-    } // end while (getch()!=current)
-    if (made_an_error==0)                         // If no error was made this time
-    P[n1][n2] = ((P[n1][n2]* 0.9) <= p_baseline) ? p_baseline : P[n1][n2] * 0.9;
-    // the prob is slowly decreased towards the baseline (i.e. uniform) value.
-  } // end while (1)
+   //printf("%s\n", cmd);
+   system(cmd);  			                     // Launch the command by a system shell call
+ 	 printf("?>    \r");  fflush(stdout);  // Prints on screen a prompt
+    //print_probs();
+    nerr = 0;
+    //while (!compare(current1, current2)) {  // Blocks execution until user press the right button on keyboard
+    while ((tmp = compare(current1, current2))!=1)
+      if (tmp==-2)
+      //printf("repeat %s\n", cmd);
+      system(cmd);
+      else {
+             printf("X>    \r");  fflush(stdout); // Feedback is given to the user
+             made_an_error = 1;      // If we are here, at least one error has been made.
+             Nerr++;                 // The counter of the total number of mistakes is updated here
+             //printf("%s\n", err);
+             system(err);            // The error sound is played to the user
+             //printf("%s\n", cmd);
+             system(cmd);            // The symbol is played again to the user
+             P[n1][n2] *= 2.;       // The corresponding occurrence probability is doubled (i.e. repetita iuvant)
+           }
+        // } // end while (getch()!=current)
+
+	  if ((tmp!=-2) && (made_an_error==0))  // If no error was made this time, and it was not a simple repetition...
+   P[n1][n2] *= 0.9;                // the prob is slowly decreased towards the baseline (i.e. uniform) value.
+	} // end while (1)
 //-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 return 0;
 } // end main
-
--- CWpairs.h
+++ CWpairs.h
@@ -1,8 +1,8 @@
 //
-// CWPairs - My own Interactive and Adaptive 
-//            Morse trainer for Single Characters
+// CWpairs -   My own Interactive and Adaptive
+//            Morse trainer for Pairs of Characters
 //
-// 22/1/2017 - (c) Michele Giugliano (mgiugliano@gmail.com)
+// 23/12/2017 - (c) Michele Giugliano (mgiugliano@gmail.com)
 //
 // CWpairs.h --> support file (gathering all include and functions)
 //
@@ -13,7 +13,7 @@
 #include <math.h>
 #include <signal.h>     // Required to capture the CTRL-C to stop (safely) the program
 
-#include <termios.h>    // Required by the function getch() 
+#include <termios.h>    // Required by the function getch() and getch2()
 #include <unistd.h>
 #include <sys/ioctl.h>
 #include <sys/types.h>
@@ -25,32 +25,31 @@ int Nsyms;              // Global var, containing the number of symbols entered 
 char **syms;            // Global var, containing the symbols entered by the user
 double **P;             // Global var, containing the presentation occurrence probabilities
 char A[1];              // Global var, containing the first of the two chars entered by the user
-char B[1];              // Global var, containing the first of the two chars entered by the user
+char B[1];              // Global var, containing the second of the two chars entered by the user
 
 //---------------------------------------------------------------------------
 // rand_event2():
-// Given an array of N positive numbers (representing probabilites, i.e. numbers in [0 ; 1], 
-// whose total sum is 1) P[i] i=1...N, this routine returns an integer i corresponding to 
+// Given an array of N positive numbers (representing probabilites, i.e. numbers in [0 ; 1],
+// whose total sum is 1) P[i] i=1...N, this routine returns an integer i corresponding to
 // the occurrence of a pseudo-random event with prob P[i].
 //
 // Imagine all P[i] as segments on a line of lenght 1, depicted as below
 // |----P1----+--P2--+----P3----+--------P4--------|
-// 
+//
 // If you now consider a pseudo random number r, uniformly distributed in the range [0 ; 1],
 // probability that a realisation will be "falling" in the interval with length P[i] is exactly P[i].
-// Therefore the following algorithm simulates the occurrence of an event of out N possible, with 
+// Therefore the following algorithm simulates the occurrence of an event of out N possible, with
 // probabilites described explicitly.
-// 
-// Note: I know P is a 2-dimensional array in this case, but it is regarded as if it is a series of 
+//
+// Note: I know P is a 2-dimensional array in this case, but it is regarded as if it is a series of
 // probabilities, each corresponding to an event (identified by two index)
-
 //---------------------------------------------------------------------------
 int rand_event2(double **P, int N) {
     const double eps = 1e-9;               // This is my definition of "zero"
     int k;                                 // Running index on all the segments
     double r=((double) rand())/(RAND_MAX); // The random number is drawn here: its instance is "r"
-    for (k=1; k<=N*N; k++) {               // (0 < r < P1) happens with probability P1 
-        r -= P[(k-1)/N][(k-1)%N];          // (P1 < r < (P1+P2)) happens with probability P2 
+    for (k=1; k<=N*N; k++) {               // (0 < r < P1) happens with probability P1
+        r -= P[(k-1)/N][(k-1)%N];          // (P1 < r < (P1+P2)) happens with probability P2
         if (r < eps) return k-1;           // etc. etc.
     }
     return -1; // -1 is an end error code
@@ -58,52 +57,93 @@ int rand_event2(double **P, int N) {
 
 
 // normaliseP2():
-// Given a 2-dim array of N*N positive numbers (representing probabilites, i.e. numbers in [0 ; 1], 
+// Given a 2-dim array of N*N positive numbers (representing probabilites, i.e. numbers in [0 ; 1],
 // whose total sum is 1) P[i][j] i,j=1...N, this routine re-normalise the individual values so that
 // their sum is 1 (so they are real probabilities and can be fed into rand_event().
 //
 void normaliseP2(double **P, int N) {
  int i, j;
  double sumP = 0.;                      // Temporary accumulator
- for (i=0; i<N; i++) for (j=0; j<N; j++) sumP += P[i][j];      // for summing each element 
+ for (i=0; i<N; i++) for (j=0; j<N; j++) sumP += P[i][j];      // for summing each element
  for (i=0; i<N; i++) for (j=0; j<N; j++) P[i][j] /= sumP;      // And then each element is divided by such a sum,
  return;                                // guaranteeing that the new values will sum to 1.
-} // end normaliseP()
+} // end normaliseP2()
 
 
-// getch2():
-// This is "blocking" the execution of the program and it returns the **two** key pressed by the user in sequence.
-// However, it does not require the user to press enter. I took it from the web while looking for the OSX 
-// version of keyboard hit - kbhit - available under windows. 
-//
-void getch2(void) {
+
+// getch():
+// This "blocks" the execution of the program and returns the key pressed, but
+// does not require the user to press enter after pressing the key. I took it
+// from the web while looking for the macOS version of keyboard hit - kbhit,
+// available under Windows.
+char getch(void) {
+ char chbuf[1];                         // Buffer for storing the (single) char selected by the user
  struct termios oldstate, newstate;     // The current state of the user terminal must be altered to
                                         // allow for the desired trick.
  tcgetattr(0, &oldstate);               // Current attributes are acquired to be restored later
  newstate = oldstate;                   // The current state is modified
- newstate.c_lflag &= ~ICANON;           // according to this and 
+ newstate.c_lflag &= ~ICANON;           // according to this and
  newstate.c_lflag &= ~ECHO;             // this flags.
  tcsetattr(0, TCSANOW, &newstate);      // The new state is set (temporarily)
- read(0, &A, 1);                        // The (first) character is finally acquired, by a low-level I/O read
- read(0, &B, 1);                        // The (second) character is finally acquired, by a low-level I/O read
- tcsetattr(0, TCSANOW, &oldstate);      // The previous state is restored
+ read(0, &chbuf, 1);                    // The character is finally acquired, by a low-level I/O read
+ tcsetattr(0, TCSANOW, &oldstate);      // The previous state is restored.
+ return chbuf[0];                       // The selected char is returned.
+} // end getch()
 
- return;                                // The selected chars are returned via A and B
+
+// getch2():
+// This "blocks" the execution of the program and returns the **two** keys pressed,
+// but does not require the user to press enter after pressing the keys. I took
+// it from the web while looking for the macOS version of keyboard hit - kbhit,
+// available under Windows.
+// Note: A and B are global variables.
+void getch2(void) {
+ //char chbuf[1];                         // Buffer for storing the (single) char selected by the user
+ struct termios oldstate, newstate;     // The current state of the user terminal must be altered to
+                                        // allow for the desired trick.
+ tcgetattr(0, &oldstate);               // Current attributes are acquired to be restored later
+ newstate = oldstate;                   // The current state is modified
+ newstate.c_lflag &= ~ICANON;           // according to this and
+ newstate.c_lflag &= ~ECHO;             // this flags.
+ tcsetattr(0, TCSANOW, &newstate);      // The new state is set (temporarily)
+ read(0, &A, 1);                        // The (first) char is finally acquired, by a low-level I/O read
+ if (A[0]!=' ')                         // then, unless the user had asked for a repetition,
+      read(0, &B, 1);                   // the second char is also acquired.
+ tcsetattr(0, TCSANOW, &oldstate);      // The previous state is restored.
+ return;                                // The selected char is returned.
 } // end getch2()
 
 
-// print_stats2():
+// compare():
+// This is handling the comparison between the current pair of character and the user entries.
+// Note: A and B are global variables.
+int compare(char a, char b) {
+ getch2();
+ if (A[0]==' ') return -2;
+ else
+ if (B[0]==' ') return -2;
+//printf("a: %c, b: %c, A: %c, B: %c\n",a,b,A[0],B[0]);
+ else
+ return ((a==A[0]) && (b==B[0]));
+} // end compare()
+
+
+// print_stats():
 // Prints the statistics and performance
 void print_stats2(void) {
     int i,j;
+    double p_baseline;
+    p_baseline = 1. / ((double) (Nsyms*Nsyms));
     normaliseP2(P, Nsyms);
     printf("%%%f correct (on %d trials)\n\n", 100.*(double)(Ntot-Nerr)/Ntot, Ntot); // Statistics is displayed
+    printf("Difficult characters for you, in this session:\n");
+
     for (i=0; i<Nsyms; i++)
-     for (j=0; j<Nsyms; j++) 
-      printf("%c%c[%f]\n", syms[i+1][0], syms[j+1][0], P[i][j]);               // Statistics is displayed
-    printf("Higher values mean they have been more difficult for you in this session.\n\n");
+         for (j=0; j<Nsyms; j++)
+          if (P[i][j]>p_baseline) printf("%c%c ", syms[i+1][0], syms[j+1][0]);  // Statistics is displayed
+          printf("\n\n\n");
     return;
-} // end print_stats2()
+} // end print_stats()
 
 
 // my_handler():
@@ -111,40 +151,10 @@ void print_stats2(void) {
 //
 void my_handler(int s){
     //printf("Caught signal %d\n",s);
-    if (s==2) {                            // "2" corresponds to CTRL-C
+	if (s==2) {                            // "2" corresponds to CTRL-C
         system("reset");                   // A system call to reset the terminal is issued (maybe not required)
         print_stats2();                     // Print statistics
-        free(P);                           // Memory allocated for P is released here
-    }
-    exit(1); 
+		free(P);                           // Memory allocated for P is released here
+	}
+    exit(1);
 } // end my_handler()
-
-// compare():
-// This is handling the comparison between the current pair of character and the user entries
-// 
-int compare(char a, char b) {
- getch2();
-//printf("a: %c, b: %c, A: %c, B: %c\n",a,b,A[0],B[0]);
- return ((a==A[0]) && (b==B[0]));
-} // end compare()
-
-// getch():
-// This is "blocking" the execution of the program and it returns the key pressed by the user.
-// However, it does not require the user to press enter after pressing the button. I took it 
-// from the web while looking for the OSX version of keyboard hit - kbhit - available under windows. 
-//
-char getch(void) {
- char chbuf[1];                         // Buffer for storing the (single) char selected by the user
- struct termios oldstate, newstate;     // The current state of the user terminal must be altered to
-                                        // allow for the desired trick.
- tcgetattr(0, &oldstate);               // Current attributes are acquired to be restored later
- newstate = oldstate;                   // The current state is modified
- newstate.c_lflag &= ~ICANON;           // according to this and 
- newstate.c_lflag &= ~ECHO;             // this flags.
- tcsetattr(0, TCSANOW, &newstate);      // The new state is set (temporarily)
- read(0, &chbuf, 1);                    // The character is finally acquired, by a low-level I/O read
- tcsetattr(0, TCSANOW, &oldstate);      // The previous state is restored
-
- return chbuf[0];                       // The selected char is returned as output of the function
-} // end getch()
-
