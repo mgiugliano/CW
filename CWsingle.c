1e66afbf7dc8190d2a9901a897f11585d92cf8d9
Michele Giugliano <mgiugliano@users.noreply.github.com>
2017-12-28 17:56:35 +0100

    Add files via upload
    Update and bug fix

--- CWsingle.c
+++ CWsingle.c
@@ -1,12 +1,12 @@
 //
-// CWsingle - My own Interactive and Adaptive 
+// CWsingle - My own Interactive and Adaptive
 //            Morse trainer for Single Characters
 //
 // 22/1/2017 - (c) Michele Giugliano (mgiugliano@gmail.com)
 // Compile with: gcc -o CWsingle CWsingle.c -O
 //
 
-#include "CWsingle.h"   
+#include "CWsingle.h"
 #define USAGE	"%s a b c d e... (list of letters/number/characters to include)"
 
 int main(int argc, char *argv[]) { // main()
@@ -14,7 +14,9 @@ int main(int argc, char *argv[]) { // main()
  char cmd[40];                  // Will contain the command to play the mp3 files of each symbol
  char err[40];                  // Will contain the command to play the error mp3 sound
  int n;                         // Index of the current symbol to play and to be copied
+ int nerr;						                // Number of subsequent errors - for voice cue
  char current;                  // Current symbol to play and to be copied
+ char temp;						               // Temporary variable to allow for repetition of a character
  int made_an_error;             // Boolean variable to distinguish wrong from right.
  struct sigaction sigIntHandler;// To catch the CTRL-C event.
 
@@ -25,66 +27,70 @@ int main(int argc, char *argv[]) { // main()
   syms  = argv;     // Global var, containing the symbols entered by the user
   Ntot  = 0;        // Global var, containing the total number of "trials"
   Nerr  = 0;        // Global var, containing the total number of "errors"
-  p_baseline = 1. / ((double) Nsyms); // Equal probabilities for each symbol (initially at least)   
+  p_baseline = 1. / ((double) Nsyms); // Equal probabilities for each symbol (initially at least)
 
 
- if (argc<2) {                // If called without argument, print the correct usage and 
-	printf(USAGE, argv[0]);		  // exit silently. Otherwise the number of symbols
+ if (argc<2) {            // If called without argument, print the correct usage and
+	printf(USAGE, argv[0]);	 // exit silently. Otherwise the number of symbols
     return 0;					        // entered is (argc - 1) and the symbols are argv[1], argv[2], ...
   }
   system("clear");
   printf("Welcome to CWsingle! (c) 2017 Michele Giugliano\n");
-  printf("Press any key to start and CTRL-C to terminate at any moment.\nSymbols for this session: ");
-  for (n=0; n<Nsyms; n++) 
+  printf("Symbols for this session (%d): ",Nsyms);
+  for (n=0; n<Nsyms; n++)
    printf("%c ", syms[n+1][0]);
-  printf("\n\n");
+  printf("\n\nPress any key to start and CTRL-C to terminate at any moment.\n\n");
 
   P    = (double *) calloc(Nsyms, sizeof(double));	// Memory allocation for P
-  for (n=0; n<Nsyms; n++) 
+  for (n=0; n<Nsyms; n++)
   	P[n] = p_baseline;       // All probabilities are initialised and set equal.
 
   // The system command to play an error sound, in case of a mistake, is prepared here:
-  sprintf(err, "afplay ./wrong.mp3"); // do NOT use & (so that the proper pause is granted)
-  
+  sprintf(err, "afplay ./wrong.mp3"); // do NOT use & (so that a proper pause is granted)
+
   sigIntHandler.sa_handler = my_handler;  // Catching the interrupts and later free the memory
   sigemptyset(&sigIntHandler.sa_mask);    // Catching the interrupts and later free the memory
   sigIntHandler.sa_flags = 0;             // Catching the interrupts and later free the memory
 
-  getch();
+  getch();    // The first character is wasted: the user presses a key to start.
   while (1) { //-=-=-= INFINITE LOOP -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
-    sigaction(SIGINT, &sigIntHandler, NULL);    // Let's prepare to catch CTRL-C (to free memory)
-    made_an_error = 0;					                // Boolean var initialised, to catch user's errors
+    sigaction(SIGINT, &sigIntHandler, NULL); // Let's prepare to catch CTRL-C (to free memory)
+    made_an_error = 0;					                  // Boolean var initialised, to catch user's errors
 
     Ntot++;                       // The counter of the total number of trials is updated here
 
-	  normaliseP(P, Nsyms);					// Normalise the symbols occurrence probabilities to sum 1
- 	  n = rand_event(P, Nsyms); 		// One letter is generated "randomly" with probabilities P[]
+	   normaliseP(P, Nsyms);				    	// Normalise the symbols occurrence probabilities to sum 1
+ 	  n = rand_event(P, Nsyms); 		  // One letter is generated "randomly" with probabilities P[]
     current = argv[n+1][0];       // The symbol is taken from the list of those provided by the user
-    
+
  		// Prepare now for the command to play audio files (with some exceptions in the file names)
-    if (current=='?')     sprintf(cmd, "afplay ./single/_.mp3 &"); 
-	  else if (current=='/')sprintf(cmd, "afplay ./single/+.mp3 &");
-    else                  sprintf(cmd, "afplay ./single/%c.mp3 &", current); 
+    if (current=='?')      sprintf(cmd, "afplay ./single/_.mp3 &");
+	   else if (current=='/') sprintf(cmd, "afplay ./single/+.mp3 &");
+    else                   sprintf(cmd, "afplay ./single/%c.mp3 &", current);
 
     //printf("%s\n", cmd);
-    system(cmd);  			             // Launch the command by a system shell call
- 	  printf("?>    \r");  fflush(stdout);// Prints on screen a prompt
-
-    while (getch()!=current) {  // Blocks execution until user press the right button on keyboard
+    system(cmd);  			                     // Launch the command by a system shell call
+ 	  printf("?>    \r");  fflush(stdout);  // Prints on screen a prompt
+    //print_probs();
+    nerr = 0;
+    while ((temp=getch())!=current) {  // Blocks execution until user press the right button on keyboard
+		  if (temp==' ')				   // The user simply asked for listening to the character again by the space bar
+				system(cmd);		    // Let's then play the character again, without generating an error
+		  else {
         printf("X>    \r");  fflush(stdout); // Feedback is given to the user
-        made_an_error = 1;      // If we are here, at least one error has been made.
-        Nerr++;                 // The counter of the total number of mistakes is updated here
-        system(err);            // The error sound is played to the user
-  	    system(cmd);						// The symbol is played again to the user
-  		  P[n] = P[n] * 2.;				// The corresponding occurrence probability is doubled (i.e. repetita iuvant)
-  		  
+        made_an_error = 1;  // If we are here, at least one error has been made.
+	      	nerr++;
+		      Nerr++;            // The counter of the total number of mistakes is updated here
+        system(err);       // The error sound is played to the user
+  	     system(cmd);						 // The symbol is played again to the user
+  		    P[n] = P[n] * 2.;				// The corresponding occurrence probability is doubled (i.e. repetita iuvant)
+	  	} // end if
 	  } // end while (getch()!=current)
-	  if (made_an_error==0)                         // If no error was made this time
-	  P[n] = ((P[n]* 0.9) <= p_baseline) ? p_baseline : P[n] * 0.9; 
+	  if ((temp!=' ') && (made_an_error==0))  // If no error was made this time, and it was not a simple repetition...
+	  //P[n] = ((P[n]* 0.9) <= p_baseline) ? p_baseline : P[n] * 0.9;
+   P[n] = P[n] * 0.9;
     // the prob is slowly decreased towards the baseline (i.e. uniform) value.
 	} // end while (1)
 //-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 return 0;
 } // end main
-
-
--- CWsingle.h
+++ CWsingle.h
@@ -1,5 +1,5 @@
 //
-// CWsingle - My own Interactive and Adaptive 
+// CWsingle -   My own Interactive and Adaptive
 //            Morse trainer for Single Characters
 //
 // 22/1/2017 - (c) Michele Giugliano (mgiugliano@gmail.com)
@@ -13,7 +13,7 @@
 #include <math.h>
 #include <signal.h>     // Required to capture the CTRL-C to stop (safely) the program
 
-#include <termios.h>    // Required by the function getch() 
+#include <termios.h>    // Required by the function getch()
 #include <unistd.h>
 #include <sys/ioctl.h>
 #include <sys/types.h>
@@ -27,61 +27,59 @@ double *P;              // Global var, containing the presentation occurrence pr
 
 //---------------------------------------------------------------------------
 // rand_event():
-// Given an array of N positive numbers (representing probabilites, i.e. numbers in [0 ; 1], 
-// whose total sum is 1) P[i] i=1...N, this routine returns an integer i corresponding to 
-// the occurrence of a pseudo-random event with prob P[i].
+// Receives an array P of numbers in [0 ; 1] (whose sum is 1), and returns i.
+// 'i' corresponds to the occurrence of the pseudo-random event with prob P[i]
 //
-// Imagine all P[i] as segments on a line of lenght 1, depicted as below
+// Algorithm:
+// Imagine all P[i] as segments on a line of total lenght 1, as depicted
 // |----P1----+--P2--+----P3----+--------P4--------|
-// 
-// If you now consider a pseudo random number r, uniformly distributed in the range [0 ; 1],
-// probability that a realisation will be "falling" in the interval with length P[i] is exactly P[i].
-// Therefore the following algorithm simulates the occurrence of an event of out N possible, with 
-// probabilites described explicitly.
-// 
-int rand_event(double *P, int N) {     
+//
+// Drawing a pseudo random number r, unif. distributed in [0 ; 1], the probability
+// that r will be "falling" in the interval with length P[i] is exactly P[i].
+// Thus, the occurrence of an event of out N possible, with known probabilites,
+// can be simulated by drawing a random number from a uniform distribution.
+//
+int rand_event(double *P, int N) {      // pointer to P, length of P[]
 	const double eps = 1e-9;               // This is my definition of "zero"
 	int k;                                 // Running index on all the segments
     double r=((double) rand())/(RAND_MAX); // The random number is drawn here: its instance is "r"
-    for (k=1; k<=N; k++) {                 // (0 < r < P1) happens with probability P1 
-    	r -= P[k-1];                       // (P1 < r < (P1+P2)) happens with probability P2 
+    for (k=1; k<=N; k++) {                 // (0 < r < P1) happens with probability P1
+    	r -= P[k-1];                       // (P1 < r < (P1+P2)) happens with probability P2
     	if (r < eps) return k-1;           // etc. etc.
     }
     return -1; // -1 is an end error code
 } // end rand_event()
 
 // normaliseP():
-// Given an array of N positive numbers (representing probabilites, i.e. numbers in [0 ; 1], 
-// whose total sum is 1) P[i] i=1...N, this routine re-normalise the individual values so that
-// their sum is 1 (so they are real probabilities and can be fed into rand_event().
+// Receives an array P of no in [0 ; 1] and renormalises it so that they sum to 1,
+// so they are real probabilities and can be fed into rand_event().
 //
 void normaliseP(double *P, int N) {
  int i;
  double sumP = 0.;                      // Temporary accumulator
- for (i=0; i<N; i++) sumP += P[i];      // for summing each element 
+ for (i=0; i<N; i++) sumP += P[i];      // for summing each element
  for (i=0; i<N; i++) P[i] /= sumP;      // And then each element is divided by such a sum,
  return;                                // guaranteeing that the new values will sum to 1.
 } // end normaliseP()
 
 
 // getch():
-// This is "blocking" the execution of the program and it returns the key pressed by the user.
-// However, it does not require the user to press enter after pressing the button. I took it 
-// from the web while looking for the OSX version of keyboard hit - kbhit - available under windows. 
-//
+// This "blocks" the execution of the program and returns the key pressed, but
+// does not require the user to press enter after pressing the key. I took it
+// from the web while looking for the macOS version of keyboard hit - kbhit,
+// available under Windows.
 char getch(void) {
  char chbuf[1];                         // Buffer for storing the (single) char selected by the user
  struct termios oldstate, newstate;     // The current state of the user terminal must be altered to
                                         // allow for the desired trick.
  tcgetattr(0, &oldstate);               // Current attributes are acquired to be restored later
  newstate = oldstate;                   // The current state is modified
- newstate.c_lflag &= ~ICANON;           // according to this and 
+ newstate.c_lflag &= ~ICANON;           // according to this and
  newstate.c_lflag &= ~ECHO;             // this flags.
  tcsetattr(0, TCSANOW, &newstate);      // The new state is set (temporarily)
  read(0, &chbuf, 1);                    // The character is finally acquired, by a low-level I/O read
- tcsetattr(0, TCSANOW, &oldstate);      // The previous state is restored
-
- return chbuf[0];                       // The selected char is returned as output of the function
+ tcsetattr(0, TCSANOW, &oldstate);      // The previous state is restored.
+ return chbuf[0];                       // The selected char is returned.
 } // end getch()
 
 
@@ -89,11 +87,30 @@ char getch(void) {
 // Prints the statistics and performance
 void print_stats(void) {
     int i;
+    double p_baseline;
+    p_baseline = 1. / ((double) Nsyms);
     normaliseP(P, Nsyms);
     printf("%%%f correct (on %d trials)\n\n", 100.*(double)(Ntot-Nerr)/Ntot, Ntot); // Statistics is displayed
-    for (i=0; i<Nsyms; i++) 
-      printf("%c[%f]\n", syms[i+1][0], P[i]);               // Statistics is displayed
-    printf("Higher values mean they have been more difficult for you in this session.\n\n");
+
+    printf("Difficult characters for you, in this session:\n");
+    for (i=0; i<Nsyms; i++)
+      if (P[i]>p_baseline) printf("%c ", syms[i+1][0]);               // Statistics is displayed
+    printf("\n\n\n");
+//    for (i=0; i<Nsyms; i++)
+//      printf("%c[%f]\n", syms[i+1][0], P[i]);               // Statistics is displayed
+//    printf("Higher values mean they have been more difficult for you in this session.\n\n");
+    return;
+} // end print_stats()
+
+
+void print_probs(void) {
+    int i;
+    for (i=0; i<Nsyms; i++)
+      printf("%f  ", P[i]);  // Print each element
+    printf("\n");
+//    for (i=0; i<Nsyms; i++)
+//      printf("%c[%f]\n", syms[i+1][0], P[i]);               // Statistics is displayed
+//    printf("Higher values mean they have been more difficult for you in this session.\n\n");
     return;
 } // end print_stats()
 
@@ -108,7 +125,5 @@ void my_handler(int s){
         print_stats();                     // Print statistics
 		free(P);                           // Memory allocated for P is released here
 	}
-    exit(1); 
+    exit(1);
 } // end my_handler()
-
-
